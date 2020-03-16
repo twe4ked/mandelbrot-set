a666dd01010b0190b529adee6747eeca2b177e4f
Odin Dutton <odindutton@gmail.com>
2020-03-16 11:49:20 +1100

    Introduce MandelbrotSet to better handle default values and scaling

--- src/lib.rs
+++ src/lib.rs
@@ -3,40 +3,65 @@ mod buffer;
 pub use buffer::Buffer;
 use num_complex::Complex;
 
-pub fn generate<F>(epsilon: f64, max_iterations: u32, mandelbrot_range: (f64, f64), mut f: F)
-where
-    F: FnMut(f64, f64, u32),
-{
-    let mut x: f64 = mandelbrot_range.0;
-    let mut y: f64 = mandelbrot_range.0;
-    let mut c: Complex<f64>;
-    let mut z: Complex<f64>;
-    let mut iterations: u32;
+pub struct MandelbrotSet {
+    epsilon: f64,
+    max_iterations: u32,
+    range: (f64, f64),
+}
 
-    while x <= mandelbrot_range.1 {
-        while y <= mandelbrot_range.1 {
-            iterations = 0;
-
-            c = Complex::new(x, y);
-            z = Complex::new(0.0, 0.0);
-
-            while z.norm() < mandelbrot_range.1 && iterations < max_iterations {
-                z = z * z + c;
-                iterations += 1;
-            }
-
-            let color = if iterations != max_iterations {
-                50 + iterations * 10 % 255
-            } else {
-                0
-            };
-
-            f(x, y, color);
-
-            y += epsilon
+impl Default for MandelbrotSet {
+    fn default() -> Self {
+        Self {
+            epsilon: 0.0001,
+            max_iterations: 20,
+            range: (-2.0, 2.0),
         }
-        y = mandelbrot_range.0;
+    }
+}
 
-        x += epsilon
+impl MandelbrotSet {
+    pub fn generate<F>(&self, mut f: F)
+    where
+        F: FnMut(f64, f64, u32),
+    {
+        let mut x: f64 = self.range.0;
+        let mut y: f64 = self.range.0;
+        let mut c: Complex<f64>;
+        let mut z: Complex<f64>;
+        let mut iterations: u32;
+
+        while x <= self.range.1 {
+            while y <= self.range.1 {
+                iterations = 0;
+
+                c = Complex::new(x, y);
+                z = Complex::new(0.0, 0.0);
+
+                while z.norm() < self.range.1 && iterations < self.max_iterations {
+                    z = z * z + c;
+                    iterations += 1;
+                }
+
+                let color = if iterations != self.max_iterations {
+                    50 + iterations * 10 % 255
+                } else {
+                    0
+                };
+
+                f(x, y, color);
+
+                y += self.epsilon
+            }
+            y = self.range.0;
+
+            x += self.epsilon
+        }
+    }
+
+    pub fn scale(&self, input: f64, y2: f64) -> f64 {
+        let x1: f64 = self.range.0;
+        let x2: f64 = self.range.1;
+        let y1: f64 = 0.0;
+        (((input - x1) * (y2 - y1)) / (x2 - x1)) + y1
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,15 @@ use std::path::Path;
 
 const WIDTH: usize = 1400;
 const HEIGHT: usize = 800;
-const EPSILON: f64 = 0.0001;
-const MAX_ITERATIONS: u32 = 20;
-const MANDELBROT_RANGE: (f64, f64) = (-2.0, 2.0);
 
 fn main() {
     let mut buffer = Buffer::new(WIDTH, HEIGHT);
 
-    generate(EPSILON, MAX_ITERATIONS, MANDELBROT_RANGE, |x, y, color| {
+    let mandelbrot_set = MandelbrotSet::default();
+    mandelbrot_set.generate(|x, y, color| {
         buffer.set_pixel(
-            scale(x, WIDTH as f64 - 1.0),
-            scale(y, HEIGHT as f64 - 1.0),
+            mandelbrot_set.scale(x, WIDTH as f64 - 1.0) as usize,
+            mandelbrot_set.scale(y, HEIGHT as f64 - 1.0) as usize,
             color,
         );
     });
@@ -29,12 +27,4 @@ fn main() {
     encoder.set_depth(png::BitDepth::Eight);
     let mut writer = encoder.write_header().unwrap();
     writer.write_image_data(buffer.buffer()).unwrap();
-}
-
-fn scale(input: f64, y2: f64) -> usize {
-    let x1: f64 = MANDELBROT_RANGE.0;
-    let x2: f64 = MANDELBROT_RANGE.1;
-    let y1: f64 = 0.0;
-
-    ((((input - x1) * (y2 - y1)) / (x2 - x1)) + y1) as usize
 }
